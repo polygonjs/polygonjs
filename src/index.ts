923ab4d92a19fda32905876a8be9da7ed1fb8a43
Guillaume Fradin <guillaumefradin@gmail.com>
2020-02-05 18:11:10 +0000

    test types exports

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,35 @@
-import {PolyScene} from 'src/engine/scene/PolyScene';
-import 'src/engine/poly/registers/All';
-import {ThreejsViewer} from './engine/viewers/Threejs';
+// import {PolyScene} from 'src/engine/scene/PolyScene';
+// import 'src/engine/poly/registers/All';
+// import {ThreejsViewer} from './engine/viewers/Threejs';
 
-const scene = new PolyScene();
-scene.root.create_node('ambient_light');
+// export {PolyScene, ThreejsViewer};
 
-scene.loading_controller.mark_as_loaded();
+export class Test {
+	constructor(num: number) {
+		console.log('num', num);
+	}
+}
 
-const perspective_camera1 = scene.root.create_node('perspective_camera');
-perspective_camera1.p.t.z.set(5);
+// const scene = new PolyScene();
+// scene.root.create_node('ambient_light');
 
-const geo1 = scene.root.create_node('geo');
-geo1.flags.display.set(true);
-const box1 = geo1.create_node('box');
-box1.flags.display.set(true);
-geo1.p.r.y.set('$F+20');
+// scene.loading_controller.mark_as_loaded();
 
-const stylesheet = document.createElement('style');
-stylesheet.innerText = 'html, body, canvas, .canvas_container {height: 100%; margin: 0px;} canvas {display: block;}';
-document.body.appendChild(stylesheet);
-const container = document.createElement('div');
-container.classList.add('canvas_container');
-document.body.appendChild(container);
-new ThreejsViewer(container, scene, perspective_camera1);
+// const perspective_camera1 = scene.root.create_node('perspective_camera');
+// perspective_camera1.p.t.z.set(5);
 
-scene.play();
+// const geo1 = scene.root.create_node('geo');
+// geo1.flags.display.set(true);
+// const box1 = geo1.create_node('box');
+// box1.flags.display.set(true);
+// geo1.p.r.y.set('$F+20');
+
+// const stylesheet = document.createElement('style');
+// stylesheet.innerText = 'html, body, canvas, .canvas_container {height: 100%; margin: 0px;} canvas {display: block;}';
+// document.body.appendChild(stylesheet);
+// const container = document.createElement('div');
+// container.classList.add('canvas_container');
+// document.body.appendChild(container);
+// new ThreejsViewer(container, scene, perspective_camera1);
+
+// scene.play();
