cf16c2637fc89daa75d0568dda7dc3568e8464ae
Guillaume Fradin <guillaumefradin@gmail.com>
2020-05-06 21:34:10 +0100

    add esbuild

--- src/engine/poly/registers/nodes/All.ts
+++ src/engine/poly/registers/nodes/All.ts
@@ -12,30 +12,14 @@ import {Poly} from '../../../Poly';
 
 export class AllNodesRegister {
 	static async run(poly: Poly) {
-		// const {AnimRegister} = await import(/* webpackChunkName: "Anim" */ './Anim');
 		AnimRegister.run(poly);
-
-		// const {CopRegister} = await import(/* webpackChunkName: "Cop" */ './Cop');
 		CopRegister.run(poly);
-
-		// const {EventRegister} = await import(/* webpackChunkName: "Event" */ './Event');
 		EventRegister.run(poly);
-
-		// const {GlRegister} = await import(/* webpackChunkName: "Gl" */ './Gl');
 		GlRegister.run(poly);
-
-		// const {JsRegister} = await import(/* webpackChunkName: "Js" */ './Js');
 		JsRegister.run(poly);
-
-		// const {MatRegister} = await import(/* webpackChunkName: "Mat" */ './Mat');
 		MatRegister.run(poly);
-
-		// const {ObjRegister} = await import(/* webpackChunkName: "Obj" */ './Obj');
 		ObjRegister.run(poly);
-
 		PostRegister.run(poly);
-
-		// const {SopRegister} = await import(/* webpackChunkName: "Sop" */ './Sop');
 		SopRegister.run(poly);
 	}
 }
