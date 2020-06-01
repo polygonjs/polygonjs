848202a1a64b3fa55b1f9247f4e5cc6041583f88
Guillaume Fradin <guillaumefradin@gmail.com>
2020-06-02 00:01:59 +0100

    update build

--- dist/src/engine/params/utils/Serializer.d.ts
+++ dist/src/engine/params/utils/Serializer.d.ts
@@ -19,7 +19,7 @@ export declare class ParamSerializer {
     constructor(param: BaseParamType);
     to_json(): ParamSerializerData;
     raw_input(): string | number | boolean | StringOrNumber3 | import("../ramp/RampValue").RampValueJson | StringOrNumber2 | StringOrNumber4 | null;
-    value(): string | number | boolean | import("../ramp/RampValue").RampValueJson | Number2 | Number3 | Number4 | null;
+    value(): string | number | boolean | Number2 | import("../ramp/RampValue").RampValueJson | Number3 | Number4 | null;
     expression(): string | undefined;
     error_message(): string | undefined;
     is_visible(): boolean;
