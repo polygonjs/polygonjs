2041f777a2887cc944282b10c82bb0d6a1177adf
Guillaume Fradin <guillaumefradin@gmail.com>
2020-08-05 01:02:29 +0100

    fog.glsl: clamp blend value between 0 and 1

--- src/engine/nodes/gl/gl/fog.glsl
+++ src/engine/nodes/gl/gl/fog.glsl
@@ -1,4 +1,5 @@
 vec3 compute_fog(vec4 mvPosition, vec3 base_color, vec3 fog_color, float near, float far) {
 	float blend = (-mvPosition.z - near) / (far - near);
+	blend = clamp(blend, 0.0, 1.0);
 	return blend * fog_color + (1.0 - blend) * base_color;
 }
