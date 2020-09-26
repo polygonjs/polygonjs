ccf559fb048727301f64b53f1cc1baf3b94b82ee
Guillaume Fradin <guillaumefradin@gmail.com>
2020-09-26 23:37:56 +0100

    add sop/attrib_from_texture params_label

--- src/engine/nodes/sop/AttribFromTexture.ts
+++ src/engine/nodes/sop/AttribFromTexture.ts
@@ -29,6 +29,12 @@ export class AttribFromTextureSopNode extends TypedSopNode<AttribFromTextureSopP
 	initialize_node() {
 		this.io.inputs.set_count(1);
 		this.io.inputs.init_inputs_cloned_state(InputCloneMode.FROM_NODE);
+
+		this.scene.dispatch_controller.on_add_listener(() => {
+			this.params.on_params_created('params_label', () => {
+				this.params.label.init([this.p.attrib]);
+			});
+		});
 	}
 
 	async cook(input_contents: CoreGroup[]) {
