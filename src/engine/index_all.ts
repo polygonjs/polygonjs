a022ebf549590760453b7feb12a729ce09125452
Guillaume Fradin <guillaumefradin@gmail.com>
2021-01-12 01:12:19 +0000

    add manifest importer to global exports

--- src/engine/index_all.ts
+++ src/engine/index_all.ts
@@ -1,7 +1,8 @@
 import {PolyScene} from './scene/PolyScene';
 import {SceneJsonImporter} from './io/json/import/Scene';
+import {SceneDataManifestImporter} from './io/manifest/import/SceneData';
 import {Poly} from './Poly';
 import {AllRegister} from './poly/registers/All';
 AllRegister.run();
 
-export {PolyScene, Poly, SceneJsonImporter};
+export {PolyScene, Poly, SceneJsonImporter, SceneDataManifestImporter};
