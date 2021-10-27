98e5be756b49cc7af070da1637cf3f69526aacb5
Guillaume Fradin <guillaumefradin@gmail.com>
2021-10-27 12:59:36 +0100

    revert eslint

--- src/modules/core/objects/Reflector.ts
+++ src/modules/core/objects/Reflector.ts
@@ -91,7 +91,7 @@ export class Reflector extends Mesh {
 	public onBeforeRender = this._onBeforeRender.bind(this);
 	private _coreRenderBlur: CoreRenderBlur;
 
-	constructor(public override geometry: BufferGeometry, private _options: ReflectorOptions) {
+	constructor(public geometry: BufferGeometry, private _options: ReflectorOptions) {
 		super();
 
 		const {width, height} = this._getRendererSize(this._options.renderer);
