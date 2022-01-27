1ae094ed1c18cd9a2a2cbf02a1c694600913ce2b
Guillaume Fradin <guillaumefradin@gmail.com>
2022-01-27 22:49:54 +0000

    add color to gl/constant

--- src/engine/nodes/gl/Constant.ts
+++ src/engine/nodes/gl/Constant.ts
@@ -5,19 +5,17 @@
  */
 import {TypedGlNode} from './_Base';
 import {ThreeToGl} from '../../../core/ThreeToGl';
-
-import {GlConnectionPointType, GL_CONNECTION_POINT_TYPES} from '../utils/io/connections/Gl';
-
-function typed_visible_options(type: GlConnectionPointType) {
-	const val = GL_CONNECTION_POINT_TYPES.indexOf(type);
-	return {visibleIf: {type: val}};
-}
-
 import {BaseParamType} from '../../params/_Base';
 import {NodeParamsConfig, ParamConfig} from '../utils/params/ParamsConfig';
 import {ShadersCollectionController} from './code/utils/ShadersCollectionController';
-import {CoreType} from '../../../core/Type';
-// import {GlConnectionsController} from './utils/GLConnectionsController';
+import {CoreType, isBooleanTrue} from '../../../core/Type';
+import {PolyDictionary} from '../../../types/GlobalTypes';
+import {GlConnectionPointType, GL_CONNECTION_POINT_TYPES} from '../utils/io/connections/Gl';
+
+function typed_visible_options(type: GlConnectionPointType, otherParamVal: PolyDictionary<number | boolean> = {}) {
+	const val = GL_CONNECTION_POINT_TYPES.indexOf(type);
+	return {visibleIf: {type: val, ...otherParamVal}};
+}
 
 class ConstantGlParamsConfig extends NodeParamsConfig {
 	type = ParamConfig.INTEGER(GL_CONNECTION_POINT_TYPES.indexOf(GlConnectionPointType.FLOAT), {
@@ -31,8 +29,11 @@ class ConstantGlParamsConfig extends NodeParamsConfig {
 	int = ParamConfig.INTEGER(0, typed_visible_options(GlConnectionPointType.INT));
 	float = ParamConfig.FLOAT(0, typed_visible_options(GlConnectionPointType.FLOAT));
 	vec2 = ParamConfig.VECTOR2([0, 0], typed_visible_options(GlConnectionPointType.VEC2));
-	vec3 = ParamConfig.VECTOR3([0, 0, 0], typed_visible_options(GlConnectionPointType.VEC3));
+	vec3 = ParamConfig.VECTOR3([0, 0, 0], typed_visible_options(GlConnectionPointType.VEC3, {asColor: false}));
+	color = ParamConfig.COLOR([0, 0, 0], typed_visible_options(GlConnectionPointType.VEC3, {asColor: true}));
 	vec4 = ParamConfig.VECTOR4([0, 0, 0, 0], typed_visible_options(GlConnectionPointType.VEC4));
+	/** @param when using vec3, use toggle on it should be a color */
+	asColor = ParamConfig.BOOLEAN(0, typed_visible_options(GlConnectionPointType.VEC3));
 }
 const ParamsConfig = new ConstantGlParamsConfig();
 export class ConstantGlNode extends TypedGlNode<ConstantGlParamsConfig> {
@@ -41,8 +42,6 @@ export class ConstantGlNode extends TypedGlNode<ConstantGlParamsConfig> {
 		return 'constant';
 	}
 	static readonly OUTPUT_NAME = 'val';
-	private _params_by_type: Map<GlConnectionPointType, BaseParamType> | undefined;
-	// protected _allow_inputs_created_from_params: boolean = false;
 	override initializeNode() {
 		this.io.connection_points.set_output_name_function((index: number) => ConstantGlNode.OUTPUT_NAME);
 		this.io.connection_points.set_expected_input_types_function(() => []);
@@ -50,7 +49,7 @@ export class ConstantGlNode extends TypedGlNode<ConstantGlParamsConfig> {
 	}
 
 	override setLines(shaders_collection_controller: ShadersCollectionController) {
-		const param = this._currentParam();
+		const param = this.currentParam();
 		if (!param) {
 			console.warn(`no param found for constant node for type '${this.pv.type}'`);
 			return;
@@ -78,25 +77,40 @@ export class ConstantGlNode extends TypedGlNode<ConstantGlParamsConfig> {
 		return connection_type;
 	}
 
-	private _currentParam(): BaseParamType {
-		this._params_by_type =
-			this._params_by_type ||
-			new Map<GlConnectionPointType, BaseParamType>([
-				[GlConnectionPointType.BOOL, this.p.bool],
-				[GlConnectionPointType.INT, this.p.int],
-				[GlConnectionPointType.FLOAT, this.p.float],
-				[GlConnectionPointType.VEC2, this.p.vec2],
-				[GlConnectionPointType.VEC3, this.p.vec3],
-				[GlConnectionPointType.VEC4, this.p.vec4],
-			]);
-		const connection_type = GL_CONNECTION_POINT_TYPES[this.pv.type];
-		return this._params_by_type.get(connection_type)!;
+	currentParam(): BaseParamType {
+		const type = GL_CONNECTION_POINT_TYPES[this.pv.type];
+		switch (type) {
+			case GlConnectionPointType.BOOL: {
+				return this.p.bool;
+			}
+			case GlConnectionPointType.INT: {
+				return this.p.int;
+			}
+			case GlConnectionPointType.FLOAT: {
+				return this.p.float;
+			}
+			case GlConnectionPointType.VEC2: {
+				return this.p.vec2;
+			}
+			case GlConnectionPointType.VEC3: {
+				if (isBooleanTrue(this.pv.asColor)) {
+					return this.p.color;
+				} else {
+					return this.p.vec3;
+				}
+			}
+			case GlConnectionPointType.VEC4: {
+				return this.p.vec4;
+			}
+		}
+		// we should never run this
+		return this.p.bool;
 	}
 	private _currentVarName(): string {
 		return this.glVarName(ConstantGlNode.OUTPUT_NAME);
 	}
 	currentValue() {
-		const param = this._currentParam();
+		const param = this.currentParam();
 		if (param) {
 			let value = ThreeToGl.any(param.value);
 			// ensure that it is an integer when needed
