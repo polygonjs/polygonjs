7d4ff7dbb9f36a384b30f2104092cf58841cb363
Guillaume Fradin <guillaumefradin@gmail.com>
2022-10-18 01:50:43 +0100

    sop/scatter: update presets

--- examples/presets/sop/Scatter.ts
+++ examples/presets/sop/Scatter.ts
@@ -4,10 +4,10 @@ import {ScatterSopNode} from '../../../src/engine/nodes/sop/Scatter';
 const scatterSopNodePresetsCollectionFactory: PresetsCollectionFactory<ScatterSopNode> = (node: ScatterSopNode) => {
 	const collection = new NodePresetsCollection();
 
-	[4, 5, 6, 7, 8, 9, 10].forEach((i) => {
+	[2, 3, 4, 5, 6, 7, 8, 9, 10].forEach((i, k) => {
 		const size = 2 ** i;
 		const preset = new BasePreset().addEntry(node.p.pointsCount, `${size} * ${size}`);
-		collection.addPreset(`${size}x${size}`, preset);
+		collection.addPreset(`${k + 1}: ${size}x${size} (=${size * size})`, preset);
 	});
 	return collection;
 };
