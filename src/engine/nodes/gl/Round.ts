67b129b0841df7701eb519e5bd65842256bd3fba
Guillaume Fradin <guillaumefradin@gmail.com>
2022-10-19 23:50:02 +0100

    fix g/round where a vec2 input would output a vec4

--- src/engine/nodes/gl/Round.ts
+++ src/engine/nodes/gl/Round.ts
@@ -11,50 +11,35 @@ import {ShadersCollectionController} from './code/utils/ShadersCollectionControl
 import {GlConnectionPointComponentsCountMap} from '../utils/io/connections/Gl';
 
 const ALL_COMPONENTS = ['x', 'y', 'z', 'w'];
-// const OUTPUT_NAME = 'round'
 
 export class RoundGlNode extends BaseNodeGlMathFunctionArg1GlNode {
 	static override type() {
 		return 'round';
 	}
 
-	// initializeNode() {
-	// 	super.initializeNode();
-	// 	this.set_named_outputs([new TypedConnectionFloat(v)]);
-	// }
-
-	// createParams() {
-	// 	this.add_param(ParamType.FLOAT, 'value', 1);
-	// }
 	// https://hub.jmonkeyengine.org/t/round-with-glsl/8186/6
-	override setLines(shaders_collection_controller: ShadersCollectionController) {
-		// const function_declaration_lines = []
+	override setLines(shadersCollectionController: ShadersCollectionController) {
+		const inputConnection = this.io.inputs.namedInputConnectionPoints()[0];
+		const value = ThreeToGl.vector2(this.variableForInput(inputConnection.name()));
 
-		// 		function_declaration_lines.push(`highp float round(float num){
-		// 	return floor(num)-fract(num);
-		// }`)
-		const input_connection = this.io.inputs.namedInputConnectionPoints()[0];
-		const value = ThreeToGl.vector2(this.variableForInput(input_connection.name()));
+		const outputConnection = this.io.outputs.namedOutputConnectionPoints()[0];
+		const outputType = outputConnection.type();
+		const varName = this.glVarName(outputConnection.name());
 
-		const output_connection = this.io.outputs.namedOutputConnectionPoints()[0];
-		const var_name = this.glVarName(output_connection.name());
-
-		const body_lines: string[] = [];
-		const lines_count = GlConnectionPointComponentsCountMap[output_connection.type()];
-		if (lines_count == 1) {
-			body_lines.push(`${output_connection.type()} ${var_name} = ${this._simple_line(value)}`);
+		const bodyLines: string[] = [];
+		const linesCount = GlConnectionPointComponentsCountMap[outputType];
+		if (linesCount == 1) {
+			bodyLines.push(`${outputType} ${varName} = ${this._singleLine(value)}`);
 		} else {
-			const simple_lines: string[] = ALL_COMPONENTS.map((c) => {
-				return this._simple_line(`${value}.${c}`);
+			const singleLines: string[] = ALL_COMPONENTS.slice(0, linesCount).map((c) => {
+				return this._singleLine(`${value}.${c}`);
 			});
-			body_lines.push(
-				`${output_connection.type()} ${var_name} = ${output_connection.type()}(${simple_lines.join(',')})`
-			);
+			bodyLines.push(`${outputType} ${varName} = ${outputType}(${singleLines.join(',')})`);
 		}
-		shaders_collection_controller.addBodyLines(this, body_lines);
+		shadersCollectionController.addBodyLines(this, bodyLines);
 	}
 
-	private _simple_line(value: string) {
+	private _singleLine(value: string) {
 		return `sign(${value})*floor(abs(${value})+0.5)`;
 	}
 }
