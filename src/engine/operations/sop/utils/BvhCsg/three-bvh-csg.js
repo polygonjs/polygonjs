5856ab19f47a104c0cd68b8861216c6ac1c5e3a7
Guillaume Fradin <guillaumefradin@gmail.com>
2022-10-24 23:30:28 +0100

    remove dependency three-bvh-csg and copy lib in core instead to try and fix double three import

--- src/engine/operations/sop/utils/BvhCsg/three-bvh-csg.js
+++ src/engine/operations/sop/utils/BvhCsg/three-bvh-csg.js
@@ -1,2 +1,9 @@
-import {SUBTRACTION, ADDITION, DIFFERENCE, INTERSECTION, Brush, Evaluator} from 'three-bvh-csg';
+import {
+	SUBTRACTION,
+	ADDITION,
+	DIFFERENCE,
+	INTERSECTION,
+	Brush,
+	Evaluator,
+} from '../../../../../core/geometry/boolean/three-bvh-csg';
 export {SUBTRACTION, ADDITION, DIFFERENCE, INTERSECTION, Brush, Evaluator};
