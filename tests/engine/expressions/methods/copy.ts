ffd4a7ed3d4bf9b8b824ba3f63ed7e13e1fc6a23
Guillaume Fradin <guillaumefradin@gmail.com>
2022-12-19 18:20:11 +0000

    add test

--- tests/engine/expressions/methods/copy.ts
+++ tests/engine/expressions/methods/copy.ts
@@ -2,6 +2,36 @@ import {Object3D} from 'three';
 import {CoreObject} from './../../../../src/core/geometry/Object';
 import {AttribClass} from '../../../../src/core/geometry/Constant';
 
+QUnit.test('copy can use default value with 1 arg', async (assert) => {
+	const geo1 = window.geo1;
+
+	const box1 = geo1.createNode('box');
+	const attribCreate1 = geo1.createNode('attribCreate');
+	const copy1 = geo1.createNode('copy');
+
+	attribCreate1.setInput(0, box1);
+	copy1.setInput(0, attribCreate1);
+
+	attribCreate1.setAttribClass(AttribClass.OBJECT);
+	attribCreate1.p.name.set('test');
+
+	attribCreate1.p.value1.set("copy('../copy1')");
+
+	copy1.p.count.set(4);
+
+	let container = await attribCreate1.compute();
+	assert.equal(CoreObject.attribValue(container.coreContent()!.objects()[0], 'test'), 0);
+
+	container = await copy1.compute();
+	assert.deepEqual(
+		container
+			.coreContent()!
+			.objects()
+			.map((o: Object3D) => CoreObject.attribValue(o, 'test')),
+		[0, 1, 2, 3]
+	);
+});
+
 QUnit.test('copy can use default value with 2 args', async (assert) => {
 	const geo1 = window.geo1;
 
@@ -32,7 +62,7 @@ QUnit.test('copy can use default value with 2 args', async (assert) => {
 	);
 });
 
-QUnit.test('copy can use default value wit 3 args', async (assert) => {
+QUnit.test('copy can use default value with 3 args', async (assert) => {
 	const geo1 = window.geo1;
 
 	const box1 = geo1.createNode('box');
