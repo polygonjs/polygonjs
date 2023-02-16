6558a76ae4870a8db0c8527a4fbe328eea9fa803
Guillaume Fradin <guillaumefradin@gmail.com>
2023-02-16 12:22:58 +0000

    CameraWebXRARMarkerTrackingSopNode not only accessible from dev

--- src/engine/poly/registers/nodes/Sop.ts
+++ src/engine/poly/registers/nodes/Sop.ts
@@ -638,9 +638,7 @@ export class SopRegister {
 		poly.registerNode(CameraProjectSopNode, CATEGORY_SOP.MODIFIER);
 		poly.registerNode(CameraRendererSopNode, CATEGORY_SOP.RENDER);
 		poly.registerNode(CameraRenderSceneSopNode, CATEGORY_SOP.RENDER);
-		if (process.env.NODE_ENV == 'development') {
-			poly.registerNode(CameraWebXRARMarkerTrackingSopNode, CATEGORY_SOP.WEBXR);
-		}
+		poly.registerNode(CameraWebXRARMarkerTrackingSopNode, CATEGORY_SOP.WEBXR);
 		poly.registerNode(CameraWebXRARSopNode, CATEGORY_SOP.WEBXR);
 		poly.registerNode(CameraWebXRVRSopNode, CATEGORY_SOP.WEBXR);
 		poly.registerNode(CapsuleSopNode, CATEGORY_SOP.PRIMITIVES);
