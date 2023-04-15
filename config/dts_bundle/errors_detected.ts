4279351be92659ed65a5dfa12e8220f18a102ded
Guillaume Fradin <guillaumefradin@gmail.com>
2023-04-15 16:34:29 +0100

    update types bundle error files

--- config/dts_bundle/errors_detected.ts
+++ config/dts_bundle/errors_detected.ts
@@ -1,62 +1,62 @@
-export const ERRORS_DETECTED = `public/bundled_types.d.ts(12,10): error TS2440: Import declaration conflicts with local declaration of 'Source'.
-public/bundled_types.d.ts(2171,15): error TS2395: Individual declarations in merged declaration 'BufferGeometry' must be all exported or all local.
-public/bundled_types.d.ts(3049,15): error TS2395: Individual declarations in merged declaration 'Raycaster' must be all exported or all local.
-public/bundled_types.d.ts(6978,21): error TS2300: Duplicate identifier 'OnDisposeCallback'.
-public/bundled_types.d.ts(7278,35): error TS2304: Cannot find name 'gp_Pnt2d'.
-public/bundled_types.d.ts(7278,46): error TS2304: Cannot find name 'Geom2d_Curve'.
-public/bundled_types.d.ts(7278,61): error TS2304: Cannot find name 'TopoDS_Vertex'.
-public/bundled_types.d.ts(7278,77): error TS2304: Cannot find name 'TopoDS_Edge'.
-public/bundled_types.d.ts(7278,91): error TS2304: Cannot find name 'TopoDS_Wire'.
-public/bundled_types.d.ts(7278,105): error TS2304: Cannot find name 'TopoDS_Shape'.
-public/bundled_types.d.ts(7293,30): error TS2304: Cannot find name 'gp_Pnt2d'.
-public/bundled_types.d.ts(7294,30): error TS2304: Cannot find name 'Geom2d_Curve'.
-public/bundled_types.d.ts(7295,28): error TS2304: Cannot find name 'TopoDS_Vertex'.
-public/bundled_types.d.ts(7296,26): error TS2304: Cannot find name 'TopoDS_Edge'.
-public/bundled_types.d.ts(7297,26): error TS2304: Cannot find name 'TopoDS_Wire'.
-public/bundled_types.d.ts(7298,26): error TS2304: Cannot find name 'TopoDS_Face'.
-public/bundled_types.d.ts(7299,27): error TS2304: Cannot find name 'TopoDS_Shell'.
-public/bundled_types.d.ts(7300,27): error TS2304: Cannot find name 'TopoDS_Solid'.
-public/bundled_types.d.ts(7301,31): error TS2304: Cannot find name 'TopoDS_CompSolid'.
-public/bundled_types.d.ts(7302,30): error TS2304: Cannot find name 'TopoDS_Compound'.
-public/bundled_types.d.ts(7353,35): error TS2304: Cannot find name 'Manifold'.
-public/bundled_types.d.ts(14522,34): error TS2315: Type 'Source' is not generic.
-public/bundled_types.d.ts(14802,21): error TS2300: Duplicate identifier 'OnDisposeCallback'.
-public/bundled_types.d.ts(20357,2): error TS2300: Duplicate identifier 'DEFINE'.
-public/bundled_types.d.ts(20358,2): error TS2300: Duplicate identifier 'BODY'.
-public/bundled_types.d.ts(20365,15): error TS2300: Duplicate identifier 'ShaderConfig'.
-public/bundled_types.d.ts(20382,15): error TS2300: Duplicate identifier 'VariableConfig'.
-public/bundled_types.d.ts(20436,21): error TS2300: Duplicate identifier 'DefinitionTraverseCallback'.
-public/bundled_types.d.ts(20561,18): error TS2320: Interface 'BaseObjNodeClassWithDisplayNode' cannot simultaneously extend types 'BaseObjNodeClass' and 'TesselationParamsObjNode'.
+export const ERRORS_DETECTED = `public/bundled_types.d.ts(11,10): error TS2440: Import declaration conflicts with local declaration of 'Source'.
+public/bundled_types.d.ts(2170,15): error TS2395: Individual declarations in merged declaration 'BufferGeometry' must be all exported or all local.
+public/bundled_types.d.ts(3048,15): error TS2395: Individual declarations in merged declaration 'Raycaster' must be all exported or all local.
+public/bundled_types.d.ts(6977,21): error TS2300: Duplicate identifier 'OnDisposeCallback'.
+public/bundled_types.d.ts(7277,35): error TS2304: Cannot find name 'gp_Pnt2d'.
+public/bundled_types.d.ts(7277,46): error TS2304: Cannot find name 'Geom2d_Curve'.
+public/bundled_types.d.ts(7277,61): error TS2304: Cannot find name 'TopoDS_Vertex'.
+public/bundled_types.d.ts(7277,77): error TS2304: Cannot find name 'TopoDS_Edge'.
+public/bundled_types.d.ts(7277,91): error TS2304: Cannot find name 'TopoDS_Wire'.
+public/bundled_types.d.ts(7277,105): error TS2304: Cannot find name 'TopoDS_Shape'.
+public/bundled_types.d.ts(7292,30): error TS2304: Cannot find name 'gp_Pnt2d'.
+public/bundled_types.d.ts(7293,30): error TS2304: Cannot find name 'Geom2d_Curve'.
+public/bundled_types.d.ts(7294,28): error TS2304: Cannot find name 'TopoDS_Vertex'.
+public/bundled_types.d.ts(7295,26): error TS2304: Cannot find name 'TopoDS_Edge'.
+public/bundled_types.d.ts(7296,26): error TS2304: Cannot find name 'TopoDS_Wire'.
+public/bundled_types.d.ts(7297,26): error TS2304: Cannot find name 'TopoDS_Face'.
+public/bundled_types.d.ts(7298,27): error TS2304: Cannot find name 'TopoDS_Shell'.
+public/bundled_types.d.ts(7299,27): error TS2304: Cannot find name 'TopoDS_Solid'.
+public/bundled_types.d.ts(7300,31): error TS2304: Cannot find name 'TopoDS_CompSolid'.
+public/bundled_types.d.ts(7301,30): error TS2304: Cannot find name 'TopoDS_Compound'.
+public/bundled_types.d.ts(7352,35): error TS2304: Cannot find name 'Manifold'.
+public/bundled_types.d.ts(14521,34): error TS2315: Type 'Source' is not generic.
+public/bundled_types.d.ts(14800,21): error TS2300: Duplicate identifier 'OnDisposeCallback'.
+public/bundled_types.d.ts(20212,2): error TS2300: Duplicate identifier 'DEFINE'.
+public/bundled_types.d.ts(20213,2): error TS2300: Duplicate identifier 'BODY'.
+public/bundled_types.d.ts(20220,15): error TS2300: Duplicate identifier 'ShaderConfig'.
+public/bundled_types.d.ts(20237,15): error TS2300: Duplicate identifier 'VariableConfig'.
+public/bundled_types.d.ts(20291,21): error TS2300: Duplicate identifier 'DefinitionTraverseCallback'.
+public/bundled_types.d.ts(20416,18): error TS2320: Interface 'BaseObjNodeClassWithDisplayNode' cannot simultaneously extend types 'BaseObjNodeClass' and 'TesselationParamsObjNode'.
   Named property 'copy_param_values' of types 'BaseObjNodeClass' and 'TesselationParamsObjNode' are not identical.
-public/bundled_types.d.ts(20561,18): error TS2320: Interface 'BaseObjNodeClassWithDisplayNode' cannot simultaneously extend types 'BaseObjNodeClass' and 'TesselationParamsObjNode'.
+public/bundled_types.d.ts(20416,18): error TS2320: Interface 'BaseObjNodeClassWithDisplayNode' cannot simultaneously extend types 'BaseObjNodeClass' and 'TesselationParamsObjNode'.
   Named property 'p' of types 'BaseObjNodeClass' and 'TesselationParamsObjNode' are not identical.
-public/bundled_types.d.ts(20561,18): error TS2320: Interface 'BaseObjNodeClassWithDisplayNode' cannot simultaneously extend types 'BaseObjNodeClass' and 'TesselationParamsObjNode'.
+public/bundled_types.d.ts(20416,18): error TS2320: Interface 'BaseObjNodeClassWithDisplayNode' cannot simultaneously extend types 'BaseObjNodeClass' and 'TesselationParamsObjNode'.
   Named property 'paramsConfig' of types 'BaseObjNodeClass' and 'TesselationParamsObjNode' are not identical.
-public/bundled_types.d.ts(20561,18): error TS2320: Interface 'BaseObjNodeClassWithDisplayNode' cannot simultaneously extend types 'BaseObjNodeClass' and 'TesselationParamsObjNode'.
+public/bundled_types.d.ts(20416,18): error TS2320: Interface 'BaseObjNodeClassWithDisplayNode' cannot simultaneously extend types 'BaseObjNodeClass' and 'TesselationParamsObjNode'.
   Named property 'pv' of types 'BaseObjNodeClass' and 'TesselationParamsObjNode' are not identical.
-public/bundled_types.d.ts(20689,2): error TS2300: Duplicate identifier 'DEFINE'.
-public/bundled_types.d.ts(20690,2): error TS2300: Duplicate identifier 'BODY'.
-public/bundled_types.d.ts(20722,21): error TS2300: Duplicate identifier 'DefinitionTraverseCallback'.
-public/bundled_types.d.ts(20748,15): error TS2300: Duplicate identifier 'VariableConfig'.
-public/bundled_types.d.ts(20810,15): error TS2300: Duplicate identifier 'ShaderConfig'.
-public/bundled_types.d.ts(21725,21): error TS2300: Duplicate identifier 'AllowedType'.
-public/bundled_types.d.ts(24980,21): error TS2300: Duplicate identifier 'AllowedType'.
-public/bundled_types.d.ts(25368,21): error TS2300: Duplicate identifier 'Listener'.
-public/bundled_types.d.ts(25401,21): error TS2300: Duplicate identifier 'AvailableItem'.
-public/bundled_types.d.ts(25856,21): error TS2300: Duplicate identifier 'AvailableItem'.
-public/bundled_types.d.ts(26448,21): error TS2300: Duplicate identifier 'Listener'.
-public/bundled_types.d.ts(26950,21): error TS2300: Duplicate identifier 'AvailableItem'.
-public/bundled_types.d.ts(27598,21): error TS2300: Duplicate identifier 'AvailableItem'.
-public/bundled_types.d.ts(28724,27): error TS2304: Cannot find name 'Geom2d_Curve'.
-public/bundled_types.d.ts(28725,21): error TS2304: Cannot find name 'TopoDS_Shape'.
-public/bundled_types.d.ts(28726,23): error TS2304: Cannot find name 'TopoDS_Shape'.
-public/bundled_types.d.ts(29794,18): error TS2395: Individual declarations in merged declaration 'BufferGeometry' must be all exported or all local.
-public/bundled_types.d.ts(29796,28): error TS2304: Cannot find name 'computeBoundsTree'.
-public/bundled_types.d.ts(29797,28): error TS2304: Cannot find name 'disposeBoundsTree'.
-public/bundled_types.d.ts(29799,18): error TS2395: Individual declarations in merged declaration 'Raycaster' must be all exported or all local.
-public/bundled_types.d.ts(30064,15): error TS2300: Duplicate identifier 'Capsule'.
-public/bundled_types.d.ts(30092,15): error TS2300: Duplicate identifier 'Capsule'.
-public/bundled_types.d.ts(39238,2): error TS2416: Property 'render' in type 'GodraysPass' is not assignable to the same property in base type 'Pass'.
+public/bundled_types.d.ts(20544,2): error TS2300: Duplicate identifier 'DEFINE'.
+public/bundled_types.d.ts(20545,2): error TS2300: Duplicate identifier 'BODY'.
+public/bundled_types.d.ts(20577,21): error TS2300: Duplicate identifier 'DefinitionTraverseCallback'.
+public/bundled_types.d.ts(20603,15): error TS2300: Duplicate identifier 'VariableConfig'.
+public/bundled_types.d.ts(20665,15): error TS2300: Duplicate identifier 'ShaderConfig'.
+public/bundled_types.d.ts(21580,21): error TS2300: Duplicate identifier 'AllowedType'.
+public/bundled_types.d.ts(24876,21): error TS2300: Duplicate identifier 'AllowedType'.
+public/bundled_types.d.ts(25265,21): error TS2300: Duplicate identifier 'Listener'.
+public/bundled_types.d.ts(25298,21): error TS2300: Duplicate identifier 'AvailableItem'.
+public/bundled_types.d.ts(25710,21): error TS2300: Duplicate identifier 'AvailableItem'.
+public/bundled_types.d.ts(26302,21): error TS2300: Duplicate identifier 'Listener'.
+public/bundled_types.d.ts(26804,21): error TS2300: Duplicate identifier 'AvailableItem'.
+public/bundled_types.d.ts(27492,21): error TS2300: Duplicate identifier 'AvailableItem'.
+public/bundled_types.d.ts(28624,27): error TS2304: Cannot find name 'Geom2d_Curve'.
+public/bundled_types.d.ts(28625,21): error TS2304: Cannot find name 'TopoDS_Shape'.
+public/bundled_types.d.ts(28626,23): error TS2304: Cannot find name 'TopoDS_Shape'.
+public/bundled_types.d.ts(29694,18): error TS2395: Individual declarations in merged declaration 'BufferGeometry' must be all exported or all local.
+public/bundled_types.d.ts(29696,28): error TS2304: Cannot find name 'computeBoundsTree'.
+public/bundled_types.d.ts(29697,28): error TS2304: Cannot find name 'disposeBoundsTree'.
+public/bundled_types.d.ts(29699,18): error TS2395: Individual declarations in merged declaration 'Raycaster' must be all exported or all local.
+public/bundled_types.d.ts(29964,15): error TS2300: Duplicate identifier 'Capsule'.
+public/bundled_types.d.ts(29992,15): error TS2300: Duplicate identifier 'Capsule'.
+public/bundled_types.d.ts(39138,2): error TS2416: Property 'render' in type 'GodraysPass' is not assignable to the same property in base type 'Pass'.
   Type '(renderer: WebGLRenderer, inputBuffer: WebGLRenderTarget, outputBuffer: WebGLRenderTarget, _deltaTime?: number | undefined, _stencilTest?: boolean | undefined) => void' is not assignable to type '(renderer: WebGLRenderer, inputBuffer: WebGLRenderTarget | null, outputBuffer: WebGLRenderTarget | null, deltaTime?: number | undefined, stencilTest?: boolean | undefined) => void'.
     Types of parameters 'renderer' and 'renderer' are incompatible.
       Type 'THREE.WebGLRenderer' is not assignable to type 'WebGLRenderer'.
@@ -106,44 +106,44 @@ public/bundled_types.d.ts(39238,2): error TS2416: Property 'render' in type 'God
                                                                                               Types of parameters 'attribute' and 'attribute' are incompatible.
                                                                                                 Type 'InterleavedBufferAttribute | BufferAttribute' is not assignable to type 'THREE.InterleavedBufferAttribute | THREE.BufferAttribute'.
                                                                                                   Type 'InterleavedBufferAttribute' is not assignable to type 'InterleavedBufferAttribute | BufferAttribute'.
-public/bundled_types.d.ts(39239,2): error TS2416: Property 'setDepthTexture' in type 'GodraysPass' is not assignable to the same property in base type 'Pass'.
+public/bundled_types.d.ts(39139,2): error TS2416: Property 'setDepthTexture' in type 'GodraysPass' is not assignable to the same property in base type 'Pass'.
   Type '(depthTexture: Texture, depthPacking?: DepthPackingStrategies | undefined) => void' is not assignable to type '(depthTexture: THREE.Texture, depthPacking?: THREE.DepthPackingStrategies | undefined) => void'.
     Types of parameters 'depthTexture' and 'depthTexture' are incompatible.
       Type 'THREE.Texture' is not assignable to type 'Texture'.
         Types of property 'matrix' are incompatible.
           Type 'THREE.Matrix3' is not assignable to type 'Matrix3'.
-public/bundled_types.d.ts(39261,63): error TS2344: Type 'GodraysPass' does not satisfy the constraint 'Pass'.
+public/bundled_types.d.ts(39161,63): error TS2344: Type 'GodraysPass' does not satisfy the constraint 'Pass'.
   Types of property 'setDepthTexture' are incompatible.
     Type '(depthTexture: Texture, depthPacking?: DepthPackingStrategies | undefined) => void' is not assignable to type '(depthTexture: THREE.Texture, depthPacking?: THREE.DepthPackingStrategies | undefined) => void'.
-public/bundled_types.d.ts(39379,2): error TS2416: Property 'scene' in type 'RenderPassWithContext' is not assignable to the same property in base type 'RenderPass'.
+public/bundled_types.d.ts(39279,2): error TS2416: Property 'scene' in type 'RenderPassWithContext' is not assignable to the same property in base type 'RenderPass'.
   Type 'Scene' is not assignable to type 'THREE.Scene'.
     Types of property 'fog' are incompatible.
       Type 'import("public/bundled_types").FogBase | null' is not assignable to type 'THREE.FogBase | null'.
         Type 'import("public/bundled_types").FogBase' is not assignable to type 'THREE.FogBase'.
           Types of property 'color' are incompatible.
             Type 'Color' is not assignable to type 'THREE.Color'.
-public/bundled_types.d.ts(39380,2): error TS2416: Property 'camera' in type 'RenderPassWithContext' is not assignable to the same property in base type 'RenderPass'.
+public/bundled_types.d.ts(39280,2): error TS2416: Property 'camera' in type 'RenderPassWithContext' is not assignable to the same property in base type 'RenderPass'.
   Type 'Camera' is not assignable to type 'THREE.Camera'.
     Types of property 'matrixWorldInverse' are incompatible.
       Type 'Matrix4' is not assignable to type 'THREE.Matrix4'.
-public/bundled_types.d.ts(39397,2): error TS2416: Property 'createPass' in type 'RenderPostNode' is not assignable to the same property in base type 'TypedPostNode<RenderPass, RenderPostParamsConfig>'.
+public/bundled_types.d.ts(39297,2): error TS2416: Property 'createPass' in type 'RenderPostNode' is not assignable to the same property in base type 'TypedPostNode<RenderPass, RenderPostParamsConfig>'.
   Type '(context: TypedPostNodeContext) => RenderPassWithContext' is not assignable to type '(context: TypedPostNodeContext) => RenderPass | RenderPass[] | undefined'.
     Type 'RenderPassWithContext' is not assignable to type 'RenderPass | RenderPass[] | undefined'.
       Type 'RenderPassWithContext' is not assignable to type 'RenderPass'.
         Types of property 'scene' are incompatible.
           Type 'Scene' is not assignable to type 'THREE.Scene'.
-public/bundled_types.d.ts(39398,2): error TS2416: Property 'updatePass' in type 'RenderPostNode' is not assignable to the same property in base type 'TypedPostNode<RenderPass, RenderPostParamsConfig>'.
+public/bundled_types.d.ts(39298,2): error TS2416: Property 'updatePass' in type 'RenderPostNode' is not assignable to the same property in base type 'TypedPostNode<RenderPass, RenderPostParamsConfig>'.
   Type '(pass: RenderPassWithContext) => void' is not assignable to type '(pass: RenderPass) => void'.
     Types of parameters 'pass' and 'pass' are incompatible.
       Property 'context' is missing in type 'RenderPass' but required in type 'RenderPassWithContext'.
-public/bundled_types.d.ts(39480,2): error TS2416: Property 'render' in type 'UpdateScenePass' is not assignable to the same property in base type 'CopyPass'.
+public/bundled_types.d.ts(39380,2): error TS2416: Property 'render' in type 'UpdateScenePass' is not assignable to the same property in base type 'CopyPass'.
   Type '(renderer: WebGLRenderer, inputBuffer: WebGLRenderTarget, outputBuffer: WebGLRenderTarget, deltaTime?: number | undefined, stencilTest?: boolean | undefined) => void' is not assignable to type '(renderer: WebGLRenderer, inputBuffer: WebGLRenderTarget | null, outputBuffer: WebGLRenderTarget | null, deltaTime?: number | undefined, stencilTest?: boolean | undefined) => void'.
     Types of parameters 'renderer' and 'renderer' are incompatible.
       Type 'THREE.WebGLRenderer' is not assignable to type 'WebGLRenderer'.
-public/bundled_types.d.ts(39499,57): error TS2344: Type 'UpdateScenePass' does not satisfy the constraint 'Pass'.
+public/bundled_types.d.ts(39399,57): error TS2344: Type 'UpdateScenePass' does not satisfy the constraint 'Pass'.
   Types of property 'render' are incompatible.
     Type '(renderer: WebGLRenderer, inputBuffer: WebGLRenderTarget, outputBuffer: WebGLRenderTarget, deltaTime?: number | undefined, stencilTest?: boolean | undefined) => void' is not assignable to type '(renderer: WebGLRenderer, inputBuffer: WebGLRenderTarget | null, outputBuffer: WebGLRenderTarget | null, deltaTime?: number | undefined, stencilTest?: boolean | undefined) => void'.
       Types of parameters 'renderer' and 'renderer' are incompatible.
         Type 'THREE.WebGLRenderer' is not assignable to type 'WebGLRenderer'.
-public/bundled_types.d.ts(45952,2): error TS2717: Subsequent property declarations must have the same type.  Property 'otherFragmentShaderCollectionController' must be of type 'JsLinesCollectionController | undefined', but here has type 'ShadersCollectionController | undefined'.
+public/bundled_types.d.ts(45854,2): error TS2717: Subsequent property declarations must have the same type.  Property 'otherFragmentShaderCollectionController' must be of type 'JsLinesCollectionController | undefined', but here has type 'ShadersCollectionController | undefined'.
 `;
