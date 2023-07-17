52db1db99583317767d88c94d0a018d01e5d71d8
Guillaume Fradin <guillaumefradin@gmail.com>
2023-07-17 19:27:33 +0100

    update imports

--- src/engine/index_all.ts
+++ src/engine/index_all.ts
@@ -1,17 +1,77 @@
 import {PolyScene} from './scene/PolyScene';
+import {Poly} from './Poly';
+// register
+import {AllRegister} from './poly/registers/All';
+import {AllExpressionsRegister} from './poly/registers/expressions/All';
+// polyNodes
+import {PolyNodeController} from './nodes/utils/poly/PolyNodeController';
+// io
 import {SceneJsonImporter} from './io/json/import/Scene';
 import {SceneDataManifestImporter} from './io/manifest/import/SceneData';
 import {ScenePlayerImporter} from './io/player/Scene';
-import {Poly} from './Poly';
-import {PolyNodeController} from './nodes/utils/poly/PolyNodeController';
-import {AllRegister} from './poly/registers/All';
+import type {SceneJsonExporterData} from './io/json/export/Scene';
+import type {ComplexParamJsonExporterData} from './nodes/utils/io/IOController';
+// viewers
+import {ThreejsViewer} from './viewers/Threejs';
+import {addStatsToViewer} from './viewers/utils/Stats';
+// core
+import {CoreGeometry} from '../core/geometry/Geometry';
+import {CoreGroup} from '../core/geometry/Group';
+import {CoreObject} from '../core/geometry/Object';
+import {CoreFeaturesController} from '../core/FeaturesController';
+// types
+import type {Number2, Number3, Number4} from '../types/GlobalTypes';
+import {JsConnectionPointType} from './nodes/utils/io/connections/Js';
+// params
+import type {ParamType} from './poly/ParamType';
+import {BooleanParam} from './params/Boolean';
+import {ColorParam} from './params/Color';
+import {FloatParam} from './params/Float';
+import {IntegerParam} from './params/Integer';
+import {NodePathParam} from './params/NodePath';
+import {ParamPathParam} from './params/ParamPath';
+import {StringParam} from './params/String';
+import {Vector2Param} from './params/Vector2';
+import {Vector3Param} from './params/Vector3';
+import {Vector4Param} from './params/Vector4';
 
 export {
 	PolyScene,
 	Poly,
+	// registers
+	AllRegister,
+	AllExpressionsRegister,
+	// polyNodes
+	PolyNodeController,
+	// io
 	SceneJsonImporter,
 	SceneDataManifestImporter,
-	AllRegister,
 	ScenePlayerImporter,
-	PolyNodeController,
+	SceneJsonExporterData,
+	ComplexParamJsonExporterData,
+	// viewers
+	ThreejsViewer,
+	addStatsToViewer,
+	// core
+	CoreGeometry,
+	CoreGroup,
+	CoreObject,
+	CoreFeaturesController,
+	// types
+	Number2,
+	Number3,
+	Number4,
+	JsConnectionPointType,
+	// params
+	ParamType,
+	BooleanParam,
+	ColorParam,
+	FloatParam,
+	IntegerParam,
+	NodePathParam,
+	ParamPathParam,
+	StringParam,
+	Vector2Param,
+	Vector3Param,
+	Vector4Param,
 };
