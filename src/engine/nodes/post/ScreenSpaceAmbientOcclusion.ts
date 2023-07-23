c590455f4747dd45751c96af1deafbf7b3ec62cc
Guillaume Fradin <guillaumefradin@gmail.com>
2023-07-23 12:38:42 +0100

    upgrade n8ao, postprocessing, three-gpu-pathtracer, three-mesh-bvh

--- src/engine/nodes/post/ScreenSpaceAmbientOcclusion.ts
+++ src/engine/nodes/post/ScreenSpaceAmbientOcclusion.ts
@@ -3,11 +3,30 @@
  *
  *
  */
+import type {Color} from 'three';
 import {TypedPostNode, TypedPostNodeContext, PostParamOptions} from './_Base';
 import {Pass} from 'postprocessing';
 import {NodeParamsConfig, ParamConfig} from '../utils/params/ParamsConfig';
 // @ts-ignore
 import {N8AOPostPass} from 'n8ao';
+
+interface N8AOPostPass extends Pass {
+	configuration: {
+		aoSamples: number;
+		aoRadius: number;
+		distanceFalloff: number;
+		intensity: number;
+		color: Color;
+		denoiseSamples: number;
+		denoiseRadius: number;
+		denoiseIterations: number;
+		logarithmicDepthBuffer: boolean;
+		screenSpaceRadius: boolean;
+		halfRef: boolean;
+		depthAwareUpsampling: boolean;
+	};
+	setDisplayMode: (mode: DisplayMode) => void;
+}
 
 enum DisplayMode {
 	COMBINED = 'Combined',
@@ -30,6 +49,12 @@ class ScreenSpaceAmbientOcclusionParamsConfig extends NodeParamsConfig {
 	color = ParamConfig.COLOR([0, 0, 0], {
 		...PostParamOptions,
 	});
+	/** @param aoSamples */
+	samples = ParamConfig.INTEGER(16, {
+		range: [0, 32],
+		rangeLocked: [true, false],
+		...PostParamOptions,
+	});
 	/** @param intensity */
 	intensity = ParamConfig.FLOAT(5, {
 		range: [0, 10],
@@ -38,13 +63,39 @@ class ScreenSpaceAmbientOcclusionParamsConfig extends NodeParamsConfig {
 	});
 	/** @param radius */
 	radius = ParamConfig.FLOAT(5, {
-		range: [0, 10],
+		range: [0, 20],
 		rangeLocked: [true, false],
 		...PostParamOptions,
 	});
 	/** @param distanceFallOff */
 	distanceFallOff = ParamConfig.FLOAT(1, {
 		range: [0, 1],
+		rangeLocked: [true, false],
+		...PostParamOptions,
+	});
+	/** @param halfRef */
+	halfRef = ParamConfig.BOOLEAN(0, {
+		...PostParamOptions,
+	});
+	/** @param screenSpaceRadius */
+	screenSpaceRadius = ParamConfig.BOOLEAN(0, {
+		...PostParamOptions,
+	});
+	/** @param denoise samples  */
+	denoiseSamples = ParamConfig.INTEGER(8, {
+		range: [0, 32],
+		rangeLocked: [true, false],
+		...PostParamOptions,
+	});
+	/** @param denoise radius */
+	denoiseRadius = ParamConfig.FLOAT(12, {
+		range: [0, 20],
+		rangeLocked: [true, false],
+		...PostParamOptions,
+	});
+	/** @param denoise iterations  */
+	denoiseIterations = ParamConfig.INTEGER(2, {
+		range: [0, 4],
 		rangeLocked: [true, false],
 		...PostParamOptions,
 	});
@@ -73,10 +124,18 @@ export class ScreenSpaceAmbientOcclusionPostNode extends TypedPostNode<Pass, Scr
 	}
 
 	override updatePass(pass: N8AOPostPass) {
+		pass.configuration.aoSamples = this.pv.samples;
 		pass.configuration.aoRadius = this.pv.radius;
 		pass.configuration.distanceFalloff = this.pv.distanceFallOff;
 		pass.configuration.intensity = this.pv.intensity;
 		pass.configuration.color.copy(this.pv.color);
+		//
+		pass.configuration.halfRef = this.pv.halfRef;
+		pass.configuration.screenSpaceRadius = this.pv.screenSpaceRadius;
+		// denoise
+		pass.configuration.denoiseSamples = this.pv.denoiseSamples;
+		pass.configuration.denoiseRadius = this.pv.denoiseRadius;
+		pass.configuration.denoiseIterations = this.pv.denoiseIterations;
 
 		const displayMode = DISPLAY_MODES[this.pv.displayMode];
 		pass.setDisplayMode(displayMode);
