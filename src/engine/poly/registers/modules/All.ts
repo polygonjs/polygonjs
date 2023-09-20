eb5a748a4f6383367059b0b1e9043f6966eeb267
Guillaume Fradin <guillaumefradin@gmail.com>
2023-09-20 14:55:02 +0100

    import gsap module

--- src/engine/poly/registers/modules/All.ts
+++ src/engine/poly/registers/modules/All.ts
@@ -1,7 +1,7 @@
 import {PolyEngine} from '../../../Poly';
 import {CADModule} from './entryPoints/CAD';
 import {CSGModule} from './entryPoints/CSG';
-// import {GSAPModule} from './entryPoints/GSAP';
+import {GSAPModule} from './entryPoints/GSAP';
 import {PBRModule} from './entryPoints/PBR';
 import {QUADModule} from './entryPoints/QUAD';
 import {SDFModule} from './entryPoints/SDF';
@@ -11,7 +11,7 @@ export class AllModulesRegister {
 	static run(poly: PolyEngine) {
 		poly.registerModule(CADModule);
 		poly.registerModule(CSGModule);
-		// poly.registerModule(GSAPModule);
+		poly.registerModule(GSAPModule);
 		poly.registerModule(PBRModule);
 		poly.registerModule(QUADModule);
 		poly.registerModule(SDFModule);
