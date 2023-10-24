8a1d7735dc09cf93fa9c647bcd873e65ac69bf8b
Guillaume Fradin <guillaumefradin@gmail.com>
2023-10-24 19:02:38 +0100

    test update

--- tests/engine/nodes/event/Throttle.ts
+++ tests/engine/nodes/event/Throttle.ts
@@ -3,33 +3,40 @@ import {CoreSleep} from '../../../../src/core/Sleep';
 import {ThrottleEventNode} from '../../../../src/engine/nodes/event/Throttle';
 import {EventContext} from '../../../../src/engine/scene/utils/events/_BaseEventsController';
 export function testenginenodeseventThrottle(qUnit: QUnit) {
+	qUnit.test('event/throttle nodes simple', async (assert) => {
+		const geo1 = window.geo1;
+		const eventsNetwork1 = geo1.createNode('eventsNetwork');
+		const throttle1 = eventsNetwork1.createNode('throttle');
 
-qUnit.test('event/throttle nodes simple', async (assert) => {
-	const geo1 = window.geo1;
-	const eventsNetwork1 = geo1.createNode('eventsNetwork');
-	const throttle1 = eventsNetwork1.createNode('throttle');
+		const eventContext: EventContext<Event> = {};
 
-	const eventContext: EventContext<Event> = {};
+		let count = 0;
+		throttle1.onDispatch(ThrottleEventNode.OUTPUT_NAME, () => {
+			count += 1;
+		});
 
-	let count = 0;
-	throttle1.onDispatch(ThrottleEventNode.OUTPUT_NAME, () => {
-		count += 1;
+		throttle1.p.time.set(100);
+		throttle1.p.leading.set(1);
+		throttle1.p.trailing.set(1);
+
+		await CoreSleep.sleep(100);
+
+		for (let i = 0; i < 21; i++) {
+			throttle1.processEvent(eventContext);
+			await CoreSleep.sleep(20);
+		}
+		assert.equal(count, 5, '5');
+
+		await CoreSleep.sleep(100);
+
+		// we reset the time, to make sure the internal debouncedFunc is reset
+		throttle1.p.time.set(5);
+		await CoreSleep.sleep(100);
+		count = 0;
+		for (let i = 0; i < 21; i++) {
+			throttle1.processEvent(eventContext);
+			await CoreSleep.sleep(10);
+		}
+		assert.equal(count, 21, '21');
 	});
-
-	for (let i = 0; i < 20; i++) {
-		throttle1.processEvent(eventContext);
-		await CoreSleep.sleep(20);
-	}
-	assert.equal(count, 5);
-
-	// we reset the time, to make sure the internal debouncedFunc is reset
-	throttle1.p.time.set(5);
-	count = 0;
-	for (let i = 0; i < 20; i++) {
-		throttle1.processEvent(eventContext);
-		await CoreSleep.sleep(10);
-	}
-	assert.equal(count, 21);
-});
-
 }
