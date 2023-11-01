9c9e16415437aabc8eaa1b94fe54ba64c6f449cd
Guillaume Fradin <guillaumefradin@gmail.com>
2023-11-01 22:56:52 +0000

    fix bug where 2 CursorMoveMonitor would conflict with one another

--- src/core/CursorMoveMonitor.ts
+++ src/core/CursorMoveMonitor.ts
@@ -1,14 +1,14 @@
 import {Ref} from '@vue/reactivity';
 import {Vector2} from 'three';
 
-const _lastCursorPos = new Vector2();
-const _currentCursorPos = new Vector2();
 const _cursorDelta = new Vector2();
-
+const _currentCursorPos = new Vector2();
 export class CursorMoveMonitor {
 	private _lastCursorPosSet: boolean = false;
 	private _movedCursorDistance: number = 0;
 	private cursorRef: Ref<Vector2> | undefined;
+	private _lastCursorPos = new Vector2();
+
 	constructor() {}
 	private _bound = {
 		pointermove: this._onPointermove.bind(this),
@@ -33,15 +33,15 @@ export class CursorMoveMonitor {
 		}
 		const cursor = this.cursorRef.value;
 		if (this._lastCursorPosSet == false) {
-			_lastCursorPos.copy(cursor);
+			this._lastCursorPos.copy(cursor);
 			this._lastCursorPosSet = true;
 		}
 		_currentCursorPos.copy(cursor);
-		_cursorDelta.copy(_currentCursorPos).sub(_lastCursorPos);
+		_cursorDelta.copy(_currentCursorPos).sub(this._lastCursorPos);
 		// we divide by 2 because the cursor is in the [-1,1] range
 		// and covering the whole screen would give a length of 2.
 		// But it's easier to think in term of [0,1] range
 		this._movedCursorDistance += _cursorDelta.manhattanLength() / 2;
-		_lastCursorPos.copy(_currentCursorPos);
+		this._lastCursorPos.copy(_currentCursorPos);
 	}
 }
